8ae4aac472316905e607ab81a6f59760a7c7477b
Alexander Hermann <ewebotah@gmail.com>
2025-09-22 01:25:44 +0200

    fix(deployment):

--- server/index.js
+++ server/index.js
@@ -18,9 +18,25 @@ const authenticate = (req, res, next) => {
   next();
 };
 
+const allowedOrigins = [
+  "http://localhost:5173", // Для разработки
+  "https://paintball-seven.vercel.app", // Ваш Vercel URL
+  "https://paintball-production.up.railway.app", // Замените на ваш фактический Vercel URL
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173", // URL фронтенда
+    origin: function (origin, callback) {
+      // Разрешаем запросы без origin (например, из мобильных приложений)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
